54b07c9fe36d7099fe1f9d49685ce8552ea9b25b
Oguzhan Tekcan <97099924+Arheky@users.noreply.github.com>
2025-10-12 23:49:58 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     laravel({
       input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -19,10 +19,12 @@ export default defineConfig({
   ],
   build: {
     target: ['es2022', 'chrome90'],
+    outDir: 'public/build',
   },
   server: {
     host: '0.0.0.0',
     port: 5173,
+    strictPort: true,
     hmr: {
       host: 'localhost',
     },
@@ -30,4 +32,8 @@ export default defineConfig({
       usePolling: true,
     },
   },
-})
+  //
+  base: command === 'build'
+    ? '/' // Production (Render) ortamında kök dizinden yükle
+    : 'http://localhost:5173/', // Local geliştirme ortamı
+}))
