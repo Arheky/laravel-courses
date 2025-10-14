ce23cee8206c3338fc1d1661eae9ffe98346cf3e
Oguzhan Tekcan <97099924+Arheky@users.noreply.github.com>
2025-10-14 17:40:29 +0300

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-window.axios = axios
+axios.defaults.withCredentials = true
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 
-window.axios.defaults.withCredentials = true
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+axios.defaults.baseURL = ''
 
-window.axios.defaults.baseURL = import.meta.env.PROD ? '' : 'http://localhost:8000'
+axios.get('/sanctum/csrf-cookie').catch(() => {
+  console.warn('⚠️ CSRF cookie alınamadı.')
+})
 
-window.axios.get('/sanctum/csrf-cookie').catch(() => console.warn('⚠️ CSRF cookie alınamadı.'))
-
-export default window.axios
+export default axios
