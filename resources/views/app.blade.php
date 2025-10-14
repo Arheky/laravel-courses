06c12a8be1cd0853785e3f581acb459b65248068
Oguzhan Tekcan <97099924+Arheky@users.noreply.github.com>
2025-10-14 20:49:42 +0300

    Update app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="app-url" content="{{ rtrim(config('app.url'), '/') }}">
+@env('local')
+  <meta name="app-url" content="{{ rtrim(config('app.url'), '/') }}">
+@endenv
     <title inertia>{{ config('app.name', 'Laravel Courses') }}</title>
 
     @routes
